8a376269799e6fdef57fabda4ec24da1af0c85c6
AGupta <agupta@boomerangfx.com>
2025-02-20 15:25:49 +0530

    `Added function name and line number to log format`

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -11,7 +11,7 @@ os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
 FILE_HANDLER = RotatingFileHandler(
     LOG_FILE, maxBytes=5 * 1024 * 1024, backupCount=5, encoding="utf-8"  # 5MB per file, keep last 5 logs
 )
-LOG_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
+LOG_FORMAT = "%(asctime)s [%(levelname)s] (%(funcName)s) %(message)s {%(filename)s:%(lineno)d}"
 
 logging.basicConfig(
     level=logging.INFO,
